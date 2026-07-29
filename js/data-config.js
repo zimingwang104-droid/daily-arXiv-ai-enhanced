b30fd6436cddebde04f48891c7a2b30c5850cbe1
yexuanlan <zimingwang104@gmail.com>
2026-07-29 10:35:30 +0800

    Update data-config.js

--- js/data-config.js
+++ js/data-config.js
@@ -12,7 +12,7 @@ const DATA_CONFIG = {
      * GitHub repository owner (username)
      * This will be replaced during GitHub Actions workflow execution
      */
-    repoOwner: 'zimingwang104-droid',
+    repoOwner: 'dw-dengwei',
 
     /**
      * GitHub repository name
